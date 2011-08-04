52be710fe500d321ad12fbf6638a7e8b9ea7bece
Higepon (MINOWA Taro) <higepon@gmail.com>
2011-08-04 16:06:42 +0900

    mu_new seems work.

--- sampling.r
+++ sampling.r
@@ -26,27 +26,49 @@ responsibility <- function(xn, k, K, pi, mu, sigma) {
   a / b;
 }
 
+# Estep works
 Estep <- function(xx, pi, mu, sigma) {
   K <- length(mu);
-  sapply(xx, function(x) {
+  apply(xx, 1, function(...) {
     sapply(1:K, function(k) {
-      responsibility(x, k, K, pi, mu, sigma);
+      responsibility(list(...)[[1]], k, K, pi, mu, sigma);
     });
   });
 }
 
 nK <- function(xx, k, K, pi, mu, sigma) {
-  sum(sapply(xx, function(x) { responsibility(x, k, K, pi, mu, sigma); }));
+  sum(apply(xx, 1, function(...) { x = list(...)[[1]]; responsibility(x, k, K, pi, mu, sigma); }));
 }
 
 muNew <- function(xx, k, K, pi, mu, sigma) {
-  nK(xx, k, K) / sum(sapply(xx, function(x) { responsibility(x, k, K, pi, mu, sigma) * x; }));
+#  colSums(apply(xx, 1, function(...) { x <- list(...)[[1]]; responsibility(x, k, K, pi, mu, sigma) * x; })) / nK(xx, k, K, pi, mu, sigma);
+  apply(xx, 1, function(...) { x <- list(...)[[1]]; responsibility(x, k, K, pi, mu, sigma) * x; });
 }
+
+xx <- matrix(c(1, 2, 3, 4, 5, 6),ncol=2, byrow=TRUE);
+Estep(xx, pi, mu, sigma);
+##            [,1]      [,2]      [,3]
+## [1,] 0.08630052 0.5957016 0.9313342
+## [2,] 0.91369948 0.4042984 0.0686658
+
+nK(xx, 1, 2, pi, mu, sigma)
+## [1] 1.613336
+
+muNew(xx, 1, 2, pi, mu, sigma)
+
+## muNew(list(c(1, 2), c(3, 4), c(5, 6)), 1, 2, pi, mu, sigma)
+## nK(list(c(1, 2), c(3, 4), c(5, 6)), 1, 2, pi, mu, sigma)
+
 
 ## sigmaNew <- function(xx, k, K, mu, muNew, sigma) {
 
 #plot(ancestralSampling(1000));
 
 ## Estep(list(c(1, 2), c(3, 4), c(5, 6)), pi, mu, sigma);
+## > Estep(list(c(1, 2), c(3, 4), c(5, 6)), pi, mu, sigma);
+##            [,1]      [,2]      [,3]
+## [1,] 0.08630052 0.5957016 0.9313342
+## [2,] 0.91369948 0.4042984 0.0686658
+
 
 ## (dmvnorm(c(3,4), mu[[2]], sigma[[2]]) * pi[[2]]) / (dmvnorm(c(3,4), mu[[1]], sigma[[1]]) * pi[[1]] + dmvnorm(c(3,4), mu[[2]], sigma[[2]]) * pi[[2]])
